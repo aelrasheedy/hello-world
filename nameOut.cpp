c08904afe93432e9c287977c147af067562dd4eb
aelrasheedy <53531803+aelrasheedy@users.noreply.github.com>
2019-07-31 14:34:55 -0500

    Create nameOut.cpp
    Prints out the user's name and greets them.

---
+++ nameOut.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+  string x;
+  cout << "Enter your name:";
+  getline(cin,x);
+  cout <<"Hello "<< x<<", what is gucci?";
+}
